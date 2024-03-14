02428abe6bf7bfc0f6568b836ffe8adeb5198f9b
zahra <z.peikani23@gmail.com>
2024-03-14 18:41:14 +0330

    create modalAddInfo

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-'use strict'
+"use strict";
+const modalAddInfo = document.getElementById("modalAddInfo");
+const addInfo = document.getElementById("btn-add");
+const close = document.getElementById("close");
+
+addInfo.addEventListener("click", () => {
+  modalAddInfo.style.display = "block";
+});
+close.addEventListener("click", () => {
+  modalAddInfo.style.display = "none";
+});
+window.addEventListener("click", (e) => {
+  if (e.target == modalAddInfo) {
+    modalAddInfo.style.display = "none";
+  }
+});
