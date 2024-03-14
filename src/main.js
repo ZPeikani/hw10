5ca0d8dca93d9a7b505600af3ff118970bc5ab32
zahra <z.peikani23@gmail.com>
2024-03-14 23:17:48 +0330

    submit form

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@
 const modalAddInfo = document.getElementById("modalAddInfo");
 const addInfo = document.getElementById("btn-add");
 const close = document.getElementById("close");
+const form = document.querySelector("form");
+const taskId = document.getElementById("id");
+let id = 0;
+let userArr = [];
 
 addInfo.addEventListener("click", () => {
   modalAddInfo.style.display = "block";
@@ -14,3 +18,19 @@ window.addEventListener("click", (e) => {
     modalAddInfo.style.display = "none";
   }
 });
+form.addEventListener("submit", submitData);
+function submitData(event) {
+  event.preventDefault();
+  const { task, priority, status, deadline } = event.target;
+  const userData = {
+    taskName: task.value,
+    taskPriority: priority.value,
+    taskStatus: status.value,
+    taskDeadLine: deadline.value,
+    id,
+  };
+  taskId.innerText = id;
+  id++;
+  userArr.push(userData);
+  // console.log(userArr);
+}
